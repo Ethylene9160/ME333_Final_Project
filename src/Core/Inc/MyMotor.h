a690a37db79daba0ee6acca99d8f3e440b546ea1
吉安 季 <1120939526@qq.com>
2023-12-17 01:16:30 +0800

    update

--- src/Core/Inc/MyMotor.h
+++ src/Core/Inc/MyMotor.h
@@ -3,6 +3,11 @@
 #define MY_MOTOR 1
 #include "usart.h"
 #include"PID.h"
+//define the id of the 4 motors
+#define LEFT_FORWARD 2
+#define RIGHT_FORWARD 1
+#define LEFT_BACKWARD 3
+#define RIGHT_BACKWARD 4
 #ifdef __cplusplus
 extern "C"{
 #endif
