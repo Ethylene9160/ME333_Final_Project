a9bc86bfb00109cae8f3252be593ff6ffaf96846
吉安 季 <1120939526@qq.com>
2023-12-28 19:23:47 +0800

    update ethy_branch

--- src/Core/Src/MyDetector.c
+++ src/Core/Src/MyDetector.c
@@ -26,25 +26,98 @@ void initAllDetectors(){
   initDetector(&rightrightDetector, GPIO_PIN_6,GPIOB);
 }
 
+uint8_t isLarge(uint8_t a, uint8_t b){
+  uint8_t A = a>>1, B = a&0b01;
+  uint8_t C = b>>1, D = b&0b01;
+  return (B&(!C)&(!D))|(A&(!C))|(A&B&(!D));  
+}
+
 //先读内侧，再往外读取。
 void detectMove(){
-  if (digitalRead(&middlemiddleDetector) == FLAG){
-    //前进
-    Forward(2.0);
-  }else if (digitalRead(&leftmiddleDetector) == FLAG){
+  // uint8_t flag = 0b00000;
+  // flag = flag | (digitalRead(&leftleftDetector)<<4);
+  // flag = flag | (digitalRead(&leftmiddleDetector) << 3);
+  // flag = flag | (digitalRead(&middlemiddleDetector) << 2);
+  // flag = flag | (digitalRead(&rightmiddleDetector) << 1);
+  // flag = flag | (digitalRead(&rightrightDetector));
+  // printf("flag: %d\n", flag);
+
+  // //直接在寄存器层面操作，提高运算效率。
+  // uint8_t leftFlag = flag>>3;       //保留最高位和次高位（左边两个传感器）
+  // uint8_t rightFlag = flag&0b00011; //保留最低位和次低位（右边两个传感器）
+
+  uint8_t leftFlag = digitalRead(&leftmiddleDetector);       //保留最高位和次高位（左边两个传感器）
+  leftFlag = leftFlag | (digitalRead(&leftleftDetector)<<1);
+  uint8_t rightFlag = digitalRead(&rightrightDetector); //保留最低位和次低位（右边两个传感器）
+  rightFlag = rightFlag | (digitalRead(&rightmiddleDetector)<<1);
+  if(isLarge(leftFlag, rightFlag)){
     //左转
     Turn_Left(2.0);
-  }else if (digitalRead(&rightmiddleDetector) == FLAG){
-    //右转
-    Turn_Right(2.0);
-  }else if (digitalRead(&leftleftDetector) == FLAG){
-    //左转
-    Turn_Left(2.0);
-  }else if (digitalRead(&rightrightDetector) == FLAG){
+  }else if(isLarge(rightFlag, leftFlag)){
     //右转
     Turn_Right(2.0);
   }else{
     //直走
     Forward(2.0);
   }
+  // if(leftFlag && !rightFlag){
+  //   //左转
+  //   Turn_Left(2.0);
+  // }else if(!leftFlag && rightFlag){
+  //   //右转
+  //   Turn_Right(2.0);
+  // }else{
+  //   //直走
+  //   Forward(2.0);
+  // }
+  return;
+  switch (flag)
+  {
+  case 0b00000:
+  case 0b00100:
+  case 0b01110:
+  case 0b11111:
+  case 0b01010:
+  case 0b10001:
+  case 0b11011:
+    //直走
+    Forward(2.0);
+    break;
+  case 0b00001:
+  case 0b00011:
+  case 0b00001:
+    //左转
+    Turn_Left(2.0);
+    Turn_Left(2.0);
+    break;
+  case 0b01000:
+  case 0b11000:
+  case 0b10000:
+    Turn_Right(2.0);
+    break;
+  default:
+    //直走
+    Forward(2.0);
+    break;
+  }
+
+  // if (digitalRead(&middlemiddleDetector) == FLAG){
+  //   //前进
+  //   Forward(2.0);
+  // }else if (digitalRead(&leftmiddleDetector) == FLAG){
+  //   //左转
+  //   Turn_Left(2.0);
+  // }else if (digitalRead(&rightmiddleDetector) == FLAG){
+  //   //右转
+  //   Turn_Right(2.0);
+  // }else if (digitalRead(&leftleftDetector) == FLAG){
+  //   //左转
+  //   Turn_Left(2.0);
+  // }else if (digitalRead(&rightrightDetector) == FLAG){
+  //   //右转
+  //   Turn_Right(2.0);
+  // }else{
+  //   //直走
+  //   Forward(2.0);
+  // }
 }
